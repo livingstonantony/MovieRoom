e2e74b94474926a3c35a6d757d3f58fbad40a51a
livingstonantonysembiyan <livingston.antony@sembiyan.in>
2026-02-08 21:51:34 +0530

    Following web sockets are added
    /chat
    /devices
    
    End point: /devices
    
    GET
    POST
    
    @Serializable
    data class DeviceDetails(
        val deviceId: String,
        val deviceName: String,
        val os: String,
        val version: String
    )

--- server/src/main/kotlin/com/ell/movieroom/models/DeviceDetails.kt
+++ server/src/main/kotlin/com/ell/movieroom/models/DeviceDetails.kt
@@ -1,3 +1,11 @@
 package com.ell.movieroom.models
 
+import kotlinx.serialization.Serializable
 
+@Serializable
+data class DeviceDetails(
+    val deviceId: String,
+    val deviceName: String,
+    val os: String,
+    val version: String
+)
