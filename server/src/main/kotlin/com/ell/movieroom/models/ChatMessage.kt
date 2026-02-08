7a68fd0e5654df646026a7a9b6f66ebaae57fbd8
livingstonantonysembiyan <livingston.antony@sembiyan.in>
2026-02-08 22:05:10 +0530

    @Serializable data class ChatMessage(val from: String, val message: String?="", val command: String?=null)
    allow null values

--- server/src/main/kotlin/com/ell/movieroom/models/ChatMessage.kt
+++ server/src/main/kotlin/com/ell/movieroom/models/ChatMessage.kt
@@ -3,7 +3,7 @@ package com.ell.movieroom.models
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ChatMessage(val from: String, val message: String)
+data class ChatMessage(val from: String, val message: String?="", val command: String?=null)
 
 @Serializable
 data class ClientMessage(val message: String) // message received from client
